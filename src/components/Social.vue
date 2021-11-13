d11a30ccf7dd9b276436c41df3d41b185e995af2
MisakaKumomi <447f.misaka@outlook.com>
2021-11-14 00:09:55 +0800

    init: initial commit using gridsome

---
+++ src/components/Social.vue
@@ -0,0 +1,19 @@
+<template>
+  <aside class="space-x-4 mx-auto w-max my-4">
+    <a href="https://twitter.com/MisakaKumomi" class="inline-block">
+      <g-image
+        src="~/assets/Twitter.svg"
+        width="50"
+        height="50"
+        class="inline-block"
+        alt="Twitter Account"
+      />
+    </a>
+  </aside>
+</template>
+
+<script>
+export default {
+  name: "Social",
+};
+</script>
